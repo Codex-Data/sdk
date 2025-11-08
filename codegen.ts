506f54f8bcdb0b8214ebe2184b99c5c465b49779
Matt Fikowski <mattfik@gmail.com>
2025-11-07 21:35:09 -0800

    Remove fragment masking.

--- codegen.ts
+++ codegen.ts
@@ -6,6 +6,9 @@ const config: CodegenConfig = {
   generates: {
     "src/sdk/generated/": {
       preset: "client",
+      presetConfig: {
+        fragmentMasking: false,
+      },
       documents:
         "src/resources/(generated_queries|generated_mutations|generated_subscriptions)/**/*",
     },
